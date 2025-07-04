4983e93f9021c0d64bed85ecfc1f88f25be7766d
Trice21 <jesusvargas021903@gmail.com>
2025-07-03 18:05:50 -0600

    Este es el commit definitivo

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template,redirect,url_for,request
-from forms import SignupForm
+from forms import SignupForm, RegisterForm
 
 app = Flask('__name__')
 app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
@@ -24,7 +24,13 @@ def show_signup():
 
 @app.route('/login', methods=["GET","POST"])
 def user_login():
-    return render_template('login.html')
+    form = RegisterForm()
+    if form.validate_on_submit():
+        email = form.Email.data
+        password = form.password.data
+        # Aquí puedes agregar la lógica de autenticación
+        return redirect(url_for('inicio'))
+    return render_template('login.html', form=form)
 
 
 
@@ -35,3 +41,10 @@ def info():
 @app.route('/inicio/usuarios')
 def inicio_usuarios(): 
     return render_template('inicio_usuarios.html')
+
+@app.route('/admin/dashboard')
+def admin_dashboard():
+    return render_template('dashboard_admin.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
